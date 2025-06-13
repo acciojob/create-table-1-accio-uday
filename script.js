9513c328c0391c4d68f2d6301d0870e805e9841e
accio-uday <udaymanideep2002@gmail.com>
2025-06-13 19:03:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+	// Get the table by ID
+	let table = document.getElementByID("sampleTable");
+
+	// Insert a new row at the top (index 0)
+	let newRow = table.insertRow(0);
+
+	// Insert cells in the new row
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+
+	// Add text to the cells
+	cell1.innerHTML = "New Cell1";
+	cell2.innerHTML = "New Cell2";
 }
